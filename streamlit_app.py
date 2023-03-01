290fb157839e417e9de8feaf35e65f93bd9ca780
UBAK1987 <126620964+UBAK1987@users.noreply.github.com>
2023-02-28 18:46:47 -0800

    Update streamlit_app.py
    Adding one more text field

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains")
 streamlit.dataframe(my_data_rows)
 
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('The user entered ', fruit_choice)
